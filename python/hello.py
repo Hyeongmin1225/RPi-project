5d06dd71884e4380c84dd7fee2f98f2891d2dcde
Hyeongmin1225 <p991225@naver.com>
2023-01-03 16:50:08 +0900

    0103

--- python/hello.py
+++ python/hello.py
@@ -1,3 +1,14 @@
+print('hello world')
 
+list_a = [1,2,3,4,5]
 
-print('Hello world')
+num = int(input('숫자를 입력해주세요.:'))
+
+if num > 10 :
+    print ('10보다 크다')
+else :
+     print('10보다 작다')
+     
+n = int(input('숫자를 입력하세요')) 
+
+for ()
