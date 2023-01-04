a00c2ff7e768dfc3c30d3a4560d80b5c587c833c
Hyeongmin1225 <p991225@naver.com>
2023-01-04 12:08:29 +0900

    0104project2

---
+++ GPIO_test/led_test2.py
@@ -0,0 +1,44 @@
+
+import RPi.GPIO as GPIO
+
+import time
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
+
+
+RED_led=12
+BLUE_led=11
+GREEN_led=13
+while 1 : 
+    color = (input('색깔을 입력해주세요:'))
+    GPIO.setup(RED_led,GPIO.OUT, initial=GPIO.LOW)
+    GPIO.setup(BLUE_led,GPIO.OUT, initial=GPIO.LOW)
+    GPIO.setup(GREEN_led,GPIO.OUT, initial=GPIO.LOW)
+    if color == 'r' :
+        GPIO.output(RED_led, GPIO.HIGH)
+        time.sleep(1)
+
+        # GPIO.output(RED_led, GPIO.LOW)
+        # time.sleep(1)
+    
+    elif color == 'b' :
+        GPIO.output(BLUE_led, GPIO.HIGH)
+        time.sleep(1)
+
+        # GPIO.output(BLUE_led, GPIO.LOW)
+        # time.sleep(1)
+    
+    elif color == 'g' :
+        GPIO.output(GREEN_led, GPIO.HIGH)
+        time.sleep(1)
+
+        # GPIO.output(GREEN_led, GPIO.LOW)
+        # time.sleep(1)
+    
+    else :
+        GPIO.output(RED_led, GPIO.LOW)
+        GPIO.output(BLUE_led, GPIO.LOW)
+        GPIO.output(GREEN_led, GPIO.LOW)
+        
+    
