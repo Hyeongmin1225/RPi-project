b97881924f5e408b3d1da542ad9007935960e8f9
Hyeongmin1225 <p991225@naver.com>
2023-01-04 14:32:33 +0900

    0104

--- GPIO_test/pwm_test2.py
+++ GPIO_test/pwm_test2.py
@@ -9,18 +9,21 @@ GPIO.setwarnings(False)
 
 GPIO.setup(LED, GPIO.OUT)
 
+Duty_led = [90,10,80,20,70,30,60,40,50,100]
 
 PWM_LED = GPIO.PWM(LED,50)
 PWM_LED.start(0)
 
-try:
-    while True : 
-        Duty_led=input('Enter Brightness(0 to 100):')
-        duty=int(Duty_led)
+try : 
+    for i in range (20) :
+        
+        duty=int(Duty_led[i])
         print('Duty rate: ',duty)
         PWM_LED.ChangeDutyCycle(duty)
+        time.sleep (0.5)
 
 finally:
     PWM_LED.stop()
-    print('Cleaning up!')
     GPIO.cleanup()
+
+
